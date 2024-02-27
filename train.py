7e621d0018ee643a7168ac9e1dbaf3c83e1d4c45
yingmuzhi <cyxscj@126.com>
2024-02-27 07:08:21 +0000

    finish train

--- train.py
+++ train.py
@@ -1,6 +1,38 @@
+import os, sys
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3, 4, 5"
+os.environ["NCCL_DEBUG"] = "INFO"
 import dataset, model
 from torch.utils.data import DataLoader
-import torch.nn as nn, torch
+import torch.nn as nn, torch, random, numpy as np
+
+def set_seed(seed: int):
+    """
+    intro:
+        set random seed, make the final results the same.
+    args:
+        :param int seed: seed value.
+    return:
+        None
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+def load_pth(load_path: str, net: torch.Tensor):
+    """
+    intro:
+        load weight files.
+    """
+    net.load_state_dict(torch.load(load_path))
+
+def save_pth(save_path: str, net: torch.Tensor):
+    """
+    intro:
+        save weight files.
+    """
+    torch.save(net.state_dict(), save_path)
+    return
 
 def validata_data():
     file_path = "/home/yingmuzhi/SpecML/src/d2o.txt"
@@ -14,6 +46,10 @@ def validata_data():
     return result
 
 def train():
+    # set seed
+    seed = 3407 # ref `https://arxiv.org/abs/2109.08203v2`
+    set_seed(seed)
+
     # dataset
     folder_path = "/home/yingmuzhi/SpecML/src/FTIR/1_data_mapping.csv"
     mydataset = dataset.one_dimension_spectrum_data(folder_path)
@@ -23,6 +59,9 @@ def train():
     # model
     net = model.Net()
     print(net)
+    # 加载预训练模型
+    # save_pth_path = "/home/yingmuzhi/SpecML/src/pth/test.pth"
+    # load_pth(save_pth, net)
 
     # train
     loss_function = nn.MSELoss()
@@ -51,6 +90,10 @@ def train():
             loss.backward()
             optimizer.step()
             print("epoch = {}, i = {}, loss = {}".format(epoch, i, loss.detach().numpy()))
+
+            # save pth
+            save_pth_path = "/home/yingmuzhi/SpecML/src/pth/test.pth"
+            save_pth(save_pth_path, net)
         
         # validate
         net.eval()
